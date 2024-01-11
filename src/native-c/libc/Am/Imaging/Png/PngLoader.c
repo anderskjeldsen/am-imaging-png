b9300e9e29952f938de7e6a57427ce400cfb8b86
Anders Kjeldsen <anders@kelson.no>
2024-01-11 23:06:20 +0100

    fixed include

--- src/native-c/libc/Am/Imaging/Png/PngLoader.c
+++ src/native-c/libc/Am/Imaging/Png/PngLoader.c
@@ -1,7 +1,7 @@
 #include <libc/core.h>
 #include <Am/Imaging/Png/PngLoader.h>
 #include <Am/Imaging/Image.h>
-#include <libc/Am/Graphics/Png/PngLoader.h>
+#include <libc/Am/Imaging/Png/PngLoader.h>
 #include <Am/Lang/Object.h>
 #include <Am/IO/File.h>
 #include <libc/core_inline_functions.h>
