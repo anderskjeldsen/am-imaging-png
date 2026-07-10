50266af139669631f9d77b5290b75c0929c8d847
anderskjeldsen <mbp2nfuqoku3zbj2pkbicrgsx24kq7p2voq5mm4dxgli7yfhvtha>
2026-07-10 13:19:29 +0200

    useless natives?

--- src/native-c/linux-ppc32/Am/Imaging/Png/PngLoader.h
+++ src/native-c/linux-ppc32/Am/Imaging/Png/PngLoader.h
@@ -1,8 +1,7 @@
+#pragma once
 #include <libc/core.h>
 #include <Am/Imaging/Png/PngLoader.h>
-#include <amigaos/Am/Imaging/Png/PngLoader.h>
-#include <Am/Lang/Object.h>
+#include <Am/Lang/ClassRef.h>
 #include <Am/Imaging/Image.h>
 #include <Am/IO/File.h>
-#include <libc/core_inline_functions.h>
-
+#include <Am/Lang/Object.h>
